45e7e48537b1fe3751bd95e17e39eac27c9262cb
Rasmus Porsager <rasmus@porsager.com>
2023-03-31 11:04:10 +0200

    add mpv.observe and improve process and socket handling

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This library spawns [mpv](https://mpv.io) and talks to it using the [JSON IPC pr
 const mpv = Mpv({
   args: [],       // Arguments to child_process.spawn,
   options: {}     // Options to child_process.spawn,
-  path: 'mpv'     // Path of mpv (defaults to mpv or mpv.exe)
+  path: 'mpv'     // Path of mpv (defaults to mpv or mpv.exe in path or cwd)
 })
 
 mpv.command('loadfile', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
@@ -20,6 +20,12 @@ mpv.on('playback-time', time =>
 await mpv.get('volume')
 
 await mpv.set('volume', 0.5)
+
+const unobserve = await mpv.observe('volume', volume => {
+  // volume is the new value
+})
+
+unobserve() // stops observing the related property
 
 mpv.process       // process from child_process.spawn
 mpv.socket        // raw tcp socket
--- index.js
+++ index.js
@@ -1,64 +1,234 @@
-const cp = require('child_process')
-    , os = require('os')
-    , events = require('events')
-    , socket = require('./socket')
-    , prexit = require('prexit')
+import cp from 'node:child_process'
+import events from 'node:events'
+import fs from 'node:fs'
+import net from 'node:net'
 
-const socketArg = '--input-ipc-server'
-const win32 = os.platform() === 'win32'
+import prexit from 'prexit'
 
-function Mpv({
+export default Mpv
+
+async function Mpv({
   args = [],
-  options = { detached: true },
-  path = win32 ? 'mpv.exe' : 'mpv'
+  options = {},
+  path = process.platform === 'win32'
+    ? fs.existsSync('mpv.exe') ? 'mpv.exe' : 'mpv'
+    : fs.existsSync('mpv') ? './mpv' : 'mpv'
 } = {}) {
-  args = args.slice(0)
-  const mpv = new events.EventEmitter()
+  const socketArg = '--input-ipc-server'
+  const socket = new net.Socket()
+  const mpv = Object.assign(new events.EventEmitter(), {
+    end,
+    status: 'stopped',
+    set: (...args) => command('set_property', ...args),
+    get: (...args) => command('get_property', ...args),
+    command,
+    observe,
+    socket
+  })
 
-  const socketPath = win32 ? '\\\\.\\pipe\\mpvsocket' : '/tmp/mpvsocket'
+  const observers = new Map()
+      , requests = new Map()
 
-  if (!args.some(x => x.startsWith(socketArg)))
-    args.push(socketArg + '=' + socketPath + Math.random().toString(36).slice(2))
+  let observeId = 0
+    , requestId = 0
+    , queue = []
 
-  if (!args.some(x => x.startsWith('--no-config')))
-    args.push('--no-config')
+  args = (args || []).slice(0)
 
-  if (!args.some(x => x.startsWith('--idle')))
-    args.push('--idle=yes')
+  const defaults = [
+    randomPath(),
+    '--audio-fallback-to-null=yes',
+    '--no-config',
+    '--idle',
+    '--msg-level=all=warn'
+  ]
 
-  if (!args.some(x => x.startsWith('--msg-level')))
-    args.push('--msg-level=all=warn')
+  defaults.forEach(a => args.some(x => x.startsWith(a.split('=')[0])) || args.push(a))
 
-  mpv.process = cp.spawn(path, args, options)
+  const socketPath = args.find(x => x.startsWith(socketArg)).slice(socketArg.length + 1)
 
-  prexit.last(() => mpv.process.kill())
+  socket
+    .unref()
+    .setEncoding('utf8')
+    .on('error', error)
+    .on('data', data)
 
-  let lastStdErr = ''
-  mpv.process.on('exit', x => x !== 0 && error(lastStdErr || x))
-  mpv.process.stdout.setEncoding('utf8')
-  mpv.process.stderr.setEncoding('utf8')
-  mpv.process.stdout.on('data', x => lastStdErr = x)
-  mpv.process.stderr.on('data', () => { /* noop - ensure drain of mpv */ })
-  mpv.process.on('error', error)
+  prexit.last(end)
 
-  mpv.socket = socket(
-    args.find(x => x.startsWith(socketArg)).slice(socketArg.length + 1),
-    err => {
-      mpv.process.kill()
-      error(err)
-    }
-  )
-  mpv.socket.on('event', (eventName, data) => mpv.emit(eventName, data))
-
-  mpv.command = mpv.socket.send
-  mpv.set = (...args) => mpv.socket.send('set_property', ...args)
-  mpv.get = (...args) => mpv.socket.send('get_property', ...args)
+  await start()
 
   return mpv
 
-  function error(x) {
-    mpv.emit('error', x)
+  function end() {
+    mpv.process.removeAllListeners()
+    kill()
+    mpv.process.unref()
   }
-}
 
-module.exports = Mpv
+  function kill() {
+    mpv.process && !mpv.process.killed && mpv.process.kill()
+  }
+
+  function error(e) {
+    mpv.status === 'started' && kill()
+  }
+
+  async function start(emit) {
+    if (mpv.status !== 'stopped')
+      return
+
+    mpv.status = 'starting'
+
+    try {
+      mpv.process && end()
+      mpv.process = cp.spawn(path, args, {
+        stdio: ['ignore', 'ignore', 'pipe'],
+        ...options
+      })
+
+      let stderr = ''
+      mpv.process.stderr.setEncoding('utf8')
+      mpv.process.stderr.on('data', x => stderr += x)
+
+      await new Promise((resolve, reject) => {
+        mpv.process.once('error', reject)
+        mpv.process.once('close', (code, signal) => reject(Object.assign(new Error(stderr || 'closed'), { code, signal })))
+        connect().then(resolve, reject)
+      })
+
+      ready()
+      mpv.status = 'started'
+      emit && mpv.emit('restarted')
+      mpv.process.on('close', () => {
+        mpv.status = 'stopped'
+        start(true).catch(e => mpv.emit('error', e))
+      })
+    } catch (error) {
+      mpv.status = 'errored'
+      throw error
+    }
+  }
+
+  async function connect({
+    connectStart = process.hrtime.bigint() / 1000000n,
+    timeout = 5000
+  } = {}) {
+    let resolve
+      , reject
+      , error
+
+    const promise = new Promise((r, e) => (resolve = r, reject = e))
+
+    socket.once('ready', resolve)
+    socket.on('error', errored)
+    socket.on('close', close)
+    socket.connect(socketPath)
+
+    return promise.finally(() => {
+      socket.off('error', errored)
+      socket.off('ready', resolve)
+      socket.off('close', close)
+    })
+
+    function errored(e) {
+      error = e
+    }
+
+    function close() {
+      process.hrtime.bigint() / 1000000n - connectStart > timeout
+        ? reject(error || new Error('Timed out'))
+        : setTimeout(() => socket.connect(socketPath), 20)
+    }
+  }
+
+  async function observe(x, fn) {
+    if (observers.has(x)) {
+      const observer = await observers[x]
+      observer.fns.add(fn)
+      return () => unobserve(x, fn)
+    }
+
+    const id = ++observeId
+    observers[x] = mpv.command('observe_property', id, x).then(result => {
+      observers.set(x, { id, fns: new Set([fn]) })
+      return result
+    })
+    return () => unobserve(x, fn)
+  }
+
+  async function unobserve(x, fn) {
+    const observer = observers.get(x)
+    observer.fns.delete(fn)
+    if (observer.fns.size === 0) {
+      observers.delete(x)
+      mpv.command('unobserve_property', observer.id).catch(e => mpv.emit('error', e))
+    }
+  }
+
+  function ready() {
+    observers.forEach(({ id }, x) => command('observe_property', id, x))
+    requests.forEach(write)
+    queue.forEach(write)
+    queue = []
+  }
+
+  function data(x) {
+    x
+    .split(/\r?\n/g)
+    .filter(x => x)
+    .map(x =>
+      JSON.parse(x.trim())
+    )
+    .forEach(handle)
+  }
+
+  function handle(x) {
+    if (x.event) {
+      return x.event === 'property-change'
+        ? observers.has(x.name) && observers.get(x.name).fns.forEach(fn => fn(x.data))
+        : mpv.emit(x.event, x)
+    }
+
+    if (!requests.has(x.request_id))
+      return x.error !== 'success' && mpv.emit('error', Object.assign(new Error(x.error), x))
+
+    const request = requests.get(x.request_id)
+    requests.delete(x.request_id)
+
+    x.error === 'success'
+      ? request.resolve(x.data)
+      : request.reject(new Error(request.args.join(' ') + ' - failed with error: ' + x.error))
+  }
+
+  function command(...args) {
+    return new Promise((resolve, reject) => {
+      const id = ++requestId
+      const request = {
+        id,
+        resolve,
+        reject,
+        args,
+        message: JSON.stringify({
+          request_id: id,
+          command: args.filter(x => x !== undefined)
+        }) + '\n'
+      }
+
+      socket.readyState === 'open'
+        ? write(request)
+        : queue.push(request)
+    })
+  }
+
+  function write(request) {
+    socket.write(request.message)
+    requests.set(request.id, request)
+  }
+
+  function randomPath() {
+    return socketArg + '='
+         + (process.platform === 'win32' ? '\\\\.\\pipe\\mpvsocket' : '/tmp/mpvsocket')
+         + Math.random().toString(36).slice(2)
+  }
+
+}
